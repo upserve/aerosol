62c9b2b64e925cae45cd6cf2559e881c14c9fb52
Todd Lunter <tlunter@gmail.com>
2013-11-08 11:38:38 -0500

    Uses newer setup style

--- spec/slugger_deploys_spec.rb
+++ spec/slugger_deploys_spec.rb
@@ -5,5 +5,24 @@ require 'spec_helper'
 describe SluggerDeploys do
   subject { SluggerDeploys }
 
+  {
+    :auto_scaling => SluggerDeploys::AutoScaling,
+    :deploy => SluggerDeploys::Deploy,
+    :launch_configuration => SluggerDeploys::LaunchConfiguration,
+    :ssh => SluggerDeploys::Connection
+  }.each do |name, klass|
+    describe ".#{name}" do
+      before { subject.send(name, :"runner_test_#{name}") { } }
+
+      it "creates a new #{klass}" do
+        klass.instances.keys.should include :"runner_test_#{name}"
+      end
+
+      it "accessible via #{klass} without a block " do
+        subject.send("#{name}s").keys.should include :"runner_test_#{name}"
+      end
+    end
+  end
+
   it { should be_an_instance_of(Module) }
 end
