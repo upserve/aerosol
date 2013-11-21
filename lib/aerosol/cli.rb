fde3d20487b4dd126d8ac31706336cf2bc8663ec
Todd Lunter <tlunter@gmail.com>
2013-11-21 15:43:46 -0500

    Switch to Aerosol. Make just SSH for now

--- lib/aerosol/cli.rb
+++ lib/aerosol/cli.rb
@@ -1,26 +1,26 @@
 require 'rubygems'
-require 'slugger_deploys'
+require 'aerosol'
 require 'clamp'
 
-class SluggerDeploys::AbstractCommand < Clamp::Command
-  option ['-f', '--file'], 'FILE', 'deploys file to read', :default => 'deploys.rb', :attribute_name => :file
+class Aerosol::AbstractCommand < Clamp::Command
+  option ['-f', '--file'], 'FILE', 'aerosol file to read', :default => 'aerosol.rb', :attribute_name => :file
 
   def execute
     if File.exist?(file)
-      SluggerDeploys.load_file = file
+      Aerosol.load_file = file
     else
-      raise 'Could not find a deploys file!'
+      raise 'Could not find an aerosol file!'
     end
   end
 end
 
-class SluggerDeploys::SshCommand < SluggerDeploys::AbstractCommand
+class Aerosol::SshCommand < Aerosol::AbstractCommand
   option ['-r', '--run'], :flag, 'run first ssh command', :attribute_name => :run_first
   parameter 'DEPLOY', 'the deploy to list commands for', :attribute_name => :deploy_name
 
   def execute
     super
-    if deploy = SluggerDeploys.deploy(deploy_name.to_sym)
+    if deploy = Aerosol.deploy(deploy_name.to_sym)
       ssh_commands = deploy.generate_ssh_commands
       raise 'No instances to ssh too!' if ssh_commands.empty?
 
@@ -35,19 +35,7 @@ class SluggerDeploys::SshCommand < SluggerDeploys::AbstractCommand
   end
 end
 
-class SluggerDeploys::AutoScalingCommand < SluggerDeploys::AbstractCommand
-  parameter 'AUTOSCALING_GROUP', 'the auto scaling group to create', :attribute_name => :autoscaling_name
-
-  def execute
-    super
-    if autoscaling = SluggerDeploys.auto_scaling(autoscaling_name.to_sym)
-      autoscaling.create
-    end
-  end
-end
-
-class SluggerDeploys::Cli < SluggerDeploys::AbstractCommand
-  subcommand ['ssh', 's'], 'Print ssh commands for latest running instances', SluggerDeploys::SshCommand
-  subcommand ['autoscaling', 'as'], 'Create autoscaling group', SluggerDeploys::AutoScalingCommand
+class Aerosol::Cli < Aerosol::AbstractCommand
+  subcommand ['ssh', 's'], 'Print ssh commands for latest running instances', Aerosol::SshCommand
 end
 
