f5bcdcf36bf251e176c546ac4972945b3a6b1acd
Todd Lunter <tlunter@gmail.com>
2013-11-21 15:33:06 -0500

    Add pry and to_s methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
+require 'pry'
 require 'aerosol'
 # Requires supporting files with custom matchers and macros, etc, in ./support/
 # and its subdirectories.
