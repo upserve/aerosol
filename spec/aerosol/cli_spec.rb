3be604fca26e4d60d11635b398d0b96520d32864
Todd Lunter <tlunter@gmail.com>
2013-12-06 16:46:54 -0500

    No need to use `bundle exec`

--- spec/aerosol/cli_spec.rb
+++ spec/aerosol/cli_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe "Aerosol CLI" do
   describe "running the most basic command" do
-    let(:command) { "bundle exec ./bin/aerosol" }
+    let(:command) { "./bin/aerosol" }
     it "should exit with 0" do
       expect(system(command)).to be_true
     end
