1ac5cf478a86209f319cadfc1f912be9686e4ee8
Tom Hulihan <hulihan.tom159@gmail.com>
2014-05-29 16:23:54 -0400

    Fix with_connection so that the host does not get overwritten.

--- lib/aerosol/connection.rb
+++ lib/aerosol/connection.rb
@@ -11,23 +11,21 @@ class Aerosol::Connection
   def with_connection(&block)
     ensure_present! :user, :host
 
-    unless self.host.is_a? String
-      host = self.host.public_hostname || self.host.private_ip_address
-    end
+    actual_host = host.is_a?(String) ? host : (host.public_hostname || host.private_ip_address)
 
     if jump
       info "connecting to gateway #{jump[:user] || user}@#{jump[:host]}"
       gateway = Net::SSH::Gateway.new(jump[:host], jump[:user] || user, :forward_agent => true)
       begin
-        info "connecting to #{user}@#{host} through gateway"
-        gateway.ssh(host, user, &block)
+        info "connecting to #{user}@#{actual_host} through gateway"
+        gateway.ssh(actual_host, user, &block)
       ensure
         info "shutting down gateway connection"
         gateway.shutdown!
       end
     else
-      info "connecting to #{user}@#{host}"
-      Net::SSH.start(host, user, &block)
+      info "connecting to #{user}@#{actual_host}"
+      Net::SSH.start(actual_host, user, &block)
     end
   end
 end
