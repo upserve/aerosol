4e5ac77f82775f04e2d23553fe62b0488498678c
Todd Lunter <tlunter@gmail.com>
2015-03-02 15:04:21 -0500

    Enable passing host into Aerosol::Connection#with_connection

--- spec/aerosol/connection_spec.rb
+++ spec/aerosol/connection_spec.rb
@@ -63,6 +63,32 @@ describe Aerosol::Connection do
           end
         end
       end
+
+      context 'when the host is passed into with_connection' do
+        let(:public_hostname) { nil }
+        let(:host) { double(:host, :public_hostname => public_hostname, :private_ip_address => '152.60.94.125') }
+        subject do
+          Aerosol::Connection.new.tap do |inst|
+            inst.name :connection_for_tim_horton
+            inst.user 'tim_horton'
+          end
+        end
+
+        context 'when the public_hostname is present' do
+          let(:public_hostname) { 'example.com' }
+          it 'returns the public_hostname' do
+            Net::SSH.should_receive(:start).with(public_hostname, 'tim_horton')
+            subject.with_connection(host)
+          end
+        end
+
+        context 'when the public_hostname is nil' do
+          it 'returns the private_ip_address' do
+            Net::SSH.should_receive(:start).with('152.60.94.125', 'tim_horton')
+            subject.with_connection(host)
+          end
+        end
+      end
     end
   end
 end
