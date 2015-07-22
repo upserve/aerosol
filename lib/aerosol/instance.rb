303f904e93f38271e2154ff6dbf6fb73232bf1f3
Todd Lunter <tlunter@gmail.com>
2015-07-22 13:00:46 -0400

    Paginate auto scaling instances

--- lib/aerosol/instance.rb
+++ lib/aerosol/instance.rb
@@ -40,10 +40,22 @@ class Aerosol::Instance
     @description ||= describe!
   end
 
+  def self.request_all_for_token(next_token)
+    options = next_token.nil? ? {} : { next_token: next_token }
+    Aerosol::AWS.auto_scaling.describe_auto_scaling_instances(options)
+  end
+
   def self.request_all
-    Aerosol::AWS.auto_scaling
-                .describe_auto_scaling_instances
-                .auto_scaling_instances
+    next_token = nil
+    instances = []
+
+    begin
+      new_instances = request_all_for_token(next_token)
+      instances.concat(new_instances.auto_scaling_instances)
+      next_token = new_instances.next_token
+    end until next_token.nil?
+
+    instances
   end
 
 private
