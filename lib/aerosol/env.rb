4561934be1b1fa97fc31e7ff32016369c0abdb21
Todd Lunter <tlunter@gmail.com>
2015-12-01 21:02:27 -0500

    Add assume_role to Env

--- lib/aerosol/env.rb
+++ lib/aerosol/env.rb
@@ -2,5 +2,19 @@
 class Aerosol::Env
   include Dockly::Util::DSL
 
+  dsl_attribute :assume_role
   dsl_class_attribute :deploy, Aerosol::Deploy, type: Array
+
+  default_value :assume_role, nil
+
+  def perform_role_assumption
+    return if assume_role.nil?
+    Aws.config.update(
+      credentials: Aws::AssumeRoleCredentials.new(
+        role_arn: assume_role,
+        role_session_name: "aerosol-#{name}",
+        client: Aerosol::AWS.sts
+      )
+    )
+  end
 end
